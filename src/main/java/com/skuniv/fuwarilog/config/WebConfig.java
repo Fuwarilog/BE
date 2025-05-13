2ab99e96b99be590b5e0626e9fd63306504b0e0c
afflogy <dlatnqls921@naver.com>
2025-05-13 16:58:18 +0900

    FEAT #2 : application/octet -> application/json 변환 converter 방식 등록

--- src/main/java/com/skuniv/fuwarilog/config/WebConfig.java
+++ src/main/java/com/skuniv/fuwarilog/config/WebConfig.java
@@ -1,11 +1,38 @@
 package com.skuniv.fuwarilog.config;
 
+import com.skuniv.fuwarilog.config.converter.OctetStreamReadMsgConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    // application/octet 변환
+    private OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    @Autowired
+    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamReadMsgConverter);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/profile/")
+                .addResourceLocations("file:uploads/profile/");
+    }
 
     @Bean
     public RestTemplate restTemplate() {
