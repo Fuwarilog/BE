35c57b39247737ffb8f268a7a62293e0be87b005
afflogy <dlatnqls921@naver.com>
2025-05-13 09:52:11 +0900

    FEAT #3 : 구글 캘린더 API quickStart 설정

--- src/main/java/com/skuniv/fuwarilog/controller/TripController.java
+++ src/main/java/com/skuniv/fuwarilog/controller/TripController.java
@@ -1,4 +1,13 @@
 package com.skuniv.fuwarilog.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Chalendar(Trip) API", description = "여행일정 관련(캘린더) API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/trips")
 public class TripController {
 }
