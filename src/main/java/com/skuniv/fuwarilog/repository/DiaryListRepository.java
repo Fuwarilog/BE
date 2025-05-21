4a9e9b6ef30a9b8abf507d2f3c799ecc69385388
afflogy <dlatnqls921@naver.com>
2025-05-21 14:25:40 +0900

    FEAT #25 : 지도 북마크 저장 다이어리 연동 형식 변경

--- src/main/java/com/skuniv/fuwarilog/repository/DiaryListRepository.java
+++ src/main/java/com/skuniv/fuwarilog/repository/DiaryListRepository.java
@@ -1,11 +1,13 @@
 package com.skuniv.fuwarilog.repository;
 
-import com.skuniv.fuwarilog.domain.Diary;
 import com.skuniv.fuwarilog.domain.DiaryList;
+import com.skuniv.fuwarilog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DiaryListRepository extends JpaRepository<DiaryList, Long> {
     List<DiaryList> findAllByDiaryId(Long diaryId);
+
 }
